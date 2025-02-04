f62646d77f5600dca9a51fea6bd0afde0de3f12b
Benjamin Schmidt <Benny@schmidtb.de>
2025-02-04 17:46:17 +0100

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Benjamin Schmidt on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
